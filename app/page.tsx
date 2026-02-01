b2b51755a5c325a8612707f5b39946bb519cc8df
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-01 00:04:12 -0800

    layout: create and style header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Header from "./ui/header";
 
 export default function Home() {
   return (
-    <p>Hello Couch to Crunch</p>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ app/ui/header.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full py-4 px-6 --color-background border-b border-gray-300">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Couch to Crunch Logo"
+            width={180}
+            height={30}
+          />
+        </Link>
+        <nav>
+            <ul className="flex space-x-6">
+                <li>
+                    <Link href="/saved">
+                        <Image
+                            src="bookmark.svg"
+                            alt="Saved Workouts"
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                    </li>
+                    <li>
+                    <Link href="">
+                        <Image
+                            src="user.svg"
+                            alt="User Profile"
+                            width={24}
+                            height={24}
+                            className="ml-2"
+                        />
+                    </Link>
+                    </li>
+            </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
