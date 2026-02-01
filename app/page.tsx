6b7cdc2382018ce41664e55a33eda05e20985b66
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-01 00:20:59 -0800

    layout: create footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./ui/footer";
 import Header from "./ui/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ app/ui/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full py-4 px-6 --color-background border-t border-gray-300 mt-8">
+            <p className="flex justify-center text-sm text-gray-600">&copy; 2026 Couch to Crunch. All rights reserved.</p>
+        </footer>
+    );
+}
