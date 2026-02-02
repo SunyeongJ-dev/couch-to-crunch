7d5e3198666a039755bff3ba4e87a2807b09f489
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-01 20:20:23 -0800

    style: change bg colours and edit className properly

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="w-full py-4 px-6 --color-background border-t border-gray-300 mt-8">
-      <p className="flex justify-center text-sm --color-text-600">
+    <footer className="w-full py-4 px-6 bg-background border-t border-text-300 mt-8">
+      <p className="flex justify-center text-sm text-text-500">
         &copy; 2026 Couch to Crunch. All rights reserved.
       </p>
     </footer>
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full py-4 px-6 --color-background border-b border-gray-300">
+    <header className="w-full py-4 px-6 bg-background border-b border-text-300">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Link href="/">
           <Image
@@ -14,29 +14,29 @@ export default function Header() {
           />
         </Link>
         <nav>
-            <ul className="flex space-x-6">
-                <li>
-                    <Link href="/saved">
-                        <Image
-                            src="bookmark.svg"
-                            alt="Saved Workouts"
-                            width={24}
-                            height={24}
-                        />
-                    </Link>
-                    </li>
-                    <li>
-                    <Link href="">
-                        <Image
-                            src="user.svg"
-                            alt="User Profile"
-                            width={24}
-                            height={24}
-                            className="ml-2"
-                        />
-                    </Link>
-                    </li>
-            </ul>
+          <ul className="flex space-x-6">
+            <li>
+              <Link href="/saved">
+                <Image
+                  src="bookmark.svg"
+                  alt="Saved Workouts"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <Image
+                  src="user.svg"
+                  alt="User Profile"
+                  width={24}
+                  height={24}
+                  className="ml-2"
+                />
+              </Link>
+            </li>
+          </ul>
         </nav>
       </div>
     </header>
