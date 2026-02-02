8ddda2a1451500d8aa4f3da1e4e23bdbb4e5112a
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-01 19:28:02 -0800

    style: add custom colours for text and apply it to footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
-    return (
-        <footer className="w-full py-4 px-6 --color-background border-t border-gray-300 mt-8">
-            <p className="flex justify-center text-sm text-gray-600">&copy; 2026 Couch to Crunch. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer className="w-full py-4 px-6 --color-background border-t border-gray-300 mt-8">
+      <p className="flex justify-center text-sm --color-text-600">
+        &copy; 2026 Couch to Crunch. All rights reserved.
+      </p>
+    </footer>
+  );
 }
