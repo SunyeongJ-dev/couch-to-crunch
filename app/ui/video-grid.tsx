be0dc181c2ebfecee01c638a8e329f2a8f7f4834
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-03 13:34:57 -0800

    feat: create video grid component

---
+++ app/ui/video-grid.tsx
@@ -0,0 +1,13 @@
+import VideoCard from "./video-card";
+import videos from "../lib/placeholder-data";
+
+export default function VideoGrid() {
+  const videoCards = videos.map((video) => (
+    <VideoCard key={video.id} video={video} />
+  ));
+  return (
+    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {videoCards}
+    </div>
+  );
+}
