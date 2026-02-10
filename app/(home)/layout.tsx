6cbfa7dacfff3c70d84050030f1cd75be35df85b
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-09 18:58:41 -0800

    chore: fix importing wrong file

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 // app/(home)/layout.tsx
 // Layout for the home page, which includes the video grid and sidebar.
 // This has an extra wrapper around the main content to add padding for the sidebar on larger screens.
-import "./page.css";
+import "../globals.css";
 
 export default function HomeLayout({
   children,
