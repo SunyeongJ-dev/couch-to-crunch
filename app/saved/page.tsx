b080243e581e420025ccff35b82b36a6d78d1820
Sunyeong Jeon <sonyaj124.ca@gmail.com>
2026-02-27 18:37:25 -0800

    fix: add loading state to show skeleton while fetching saved videos

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -10,6 +10,7 @@ import VideoGridSkeleton from "../ui/video-grid-skeleton";
 export default function SavedPage() {
   const { videoIds, isReady } = useSavedVideo("");
   const [videos, setVideos] = useState<VideoData[]>([]);
+  const [loading, setLoading] = useState(false);
 
   // Whenever the list of saved video IDs changes, we need to fetch their details to display them.
   useEffect(() => {
@@ -20,16 +21,18 @@ export default function SavedPage() {
         return;
       }
 
+      setLoading(true);
       // Calls /api/videos GET route with ids query to fetch only videos with the specified IDs.
       const res = await fetch("/api/videos?ids=" + videoIds.join(","));
       const data = (await res.json()) as VideoData[];
       setVideos(data);
+      setLoading(false);
     }
 
     load();
   }, [videoIds]);
 
-  if (!isReady) {
+  if (!isReady || loading) {
     return (
       <div className="flex w-full flex-col max-w-7xl mx-auto px-4 pt-2 pb-6 sm:px-8">
         <h1 className="text-2xl font-bold mb-8">Saved Videos</h1>
